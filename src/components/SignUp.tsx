e9afb3e0bba793a4340264bf08da7cffe49ec186
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2023-02-26 22:29:27 -0500

    Draft wyre SignUp

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('https://api.sendwyre.com/v3/users', {
+        email,
+        password,
+        type: 'individual',
+        country: 'US',
+        lang: 'en',
+      });
+
+      console.log(response.data);
+      // Navigate the user to the login page
+      navigate('/login');
+    } catch (error) {
+      console.error(error);
+      // TODO: Handle error creating user
+    }
+  };
+
   return (
     <div>
       <h1>Sign up for our website</h1>
-      <form>
-        {/* form fields */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign up</button>
       </form>
-      <p>Already have an account? <Link to="/login">Log in here</Link></p>
     </div>
   );
 };
