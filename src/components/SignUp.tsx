ac860708eb17de5fdca0a5a3840792fbff886c10
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2023-02-28 20:04:53 -0500

    Cleaned up SignUp form

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './SignUp.css';
 
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [street1, setStreet1] = useState('');
@@ -21,6 +24,11 @@ const SignUp: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
 
     try {
       const response = await axios.post('https://api.sendwyre.com/v3/users', {
@@ -63,8 +71,8 @@ const SignUp: React.FC = () => {
 
   return (
     <div className="signup-widget">
-      <h1>Create an Account!</h1>
       <form onSubmit={handleSubmit}>
+        <h1>Create an Account!</h1>
         <div>
           <label htmlFor="email">Email:</label>
           <input
@@ -75,33 +83,58 @@ const SignUp: React.FC = () => {
             required
           />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-            required
-          />
+        <div className="password-form-row">
+          <div className="password-form-col">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </div>
+          <div className="password-form-col">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
+              required
+            />
+          </div>
+        </div>
+        <div className="password-form-row">
+          <div className="password-form-col">
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              required
+            />
+          </div>
+          <div className="password-form-col">
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              required
+            />
+          </div>
         </div>
         <div>
-          <label htmlFor="firstName">First name:</label>
-          <input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={event => setFirstName(event.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last name:</label>
-          <input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={event => setLastName(event.target.value)}
+          <label htmlFor="dateOfBirth">Date of Birth:</label>
+          <DatePicker
+            id="dateOfBirth"
+            selected={dateOfBirth ? new Date(dateOfBirth) : null}
+            onChange={(date: Date) => setDateOfBirth(date.toISOString())}
+            dateFormat="MM/dd/yyyy"
+            showYearDropdown
             required
           />
         </div>
@@ -125,86 +158,88 @@ const SignUp: React.FC = () => {
               onChange={event => setStreet2(event.target.value)}
             />
           </div>
-          <div>
-            <label htmlFor="city">City:</label>
-            <input
-              type="text"
-              id="City"
-              value={city}
-              onChange={event => setCity(event.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="state">State:</label>
-            <select
-              id="state"
-              value={state}
-              onChange={event => setState(event.target.value)}
-              required
-            >
-              <option value="">-- Select a state --</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="postalCode">Postal Code:</label>
-            <input
-              type="text"
-              id="postalCode"
-              value={postalCode}
-              onChange={event => setPostalCode(event.target.value)}
-              required
-            />
+          <div className="form-row">
+            <div>
+              <label htmlFor="city">City:</label>
+              <input
+                type="text"
+                id="City"
+                value={city}
+                onChange={event => setCity(event.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="state">State:</label>
+              <select
+                id="state"
+                value={state}
+                onChange={event => setState(event.target.value)}
+                required
+              >
+                <option value="">-- Select a state --</option>
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="FL">Florida</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="KS">Kansas</option>
+                <option value="KY">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="postalCode">Postal Code:</label>
+              <input
+                type="text"
+                id="postalCode"
+                value={postalCode}
+                onChange={event => setPostalCode(event.target.value)}
+                required
+              />
+            </div>
           </div>
           <div>
             <label htmlFor="country">Country:</label>
@@ -236,16 +271,6 @@ const SignUp: React.FC = () => {
             </select>
           </div>
         </div>      
-        <div>
-          <label htmlFor="dateOfBirth">Date of Birth:</label>
-          <input
-            type="text"
-            id="dateOfBirth"
-            value={dateOfBirth}
-            onChange={event => setDateOfBirth(event.target.value)}
-            required
-          />
-        </div>
         <button type="submit">Sign up</button>
       </form>
     </div>
